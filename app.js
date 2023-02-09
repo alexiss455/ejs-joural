538668db51f6ac55f6960fd0891df3916c8f33e2
Alexiess <alexissmanalastas@gmail.com>
2023-02-09 13:39:10 +0800

    {

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb+srv://Alexiess:gagoka45@alexiess.9vhaijd.mongodb.net/Blogdb", { useNewUrlParser: true, useUnifiedTopology: true}, function(err) {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -17,14 +27,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-let posts = [];
+  
+const blogSchema = new mongoose.Schema({
+  title: String,
+  posted: String
+});
+const blogPost = mongoose.model("blogPost", blogSchema);
 
-              
 app.get("/", function(req, res){
-  res.render("journal", {
-    startingContent: homeStartingContent,
-    postsEJS: posts
-    });
+
+ blogPost.find({}, function(err, posts){
+  if(!err){
+    res.render("journal", {postsEJS: posts, startingContent: homeStartingContent})
+  }else{
+    console.log(err);
+  }
+ })
 });
 
 app.get("/about", function(req, res){
@@ -44,42 +62,32 @@ app.post("/compose", function(req, res){
     title: req.body.inputPose,
     content: req.body.text
   };
-  posts.push(post);
-
-  res.redirect("/");
+const blogs = new blogPost({
+  title: post.title,
+  posted: post.content
+});
+blogs.save(function(err){
+if(!err){
+  console.log("SUCCESS")
+  res.redirect("/")
+}else{
+  console.log("FAIL")
+}
+});
 
 });
+
 app.get("/posts/:postName", function(req, res){
-  
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(par){
-
-    const storedTitle = _.lowerCase(par.title);
-
-    if ( requestedTitle == storedTitle) {
-
-      res.render("post", {font: par.title, fontp: par.content
-
-      });
+  const requestedTitle = req.params.postName;
+  console.log(requestedTitle);
+  blogPost.findOne({title: requestedTitle}, function(err, post){
+    if(!err){
+      res.render("post", {font: post.title, fontp: post.posted});
+    }else{
+      console.log(err)
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
